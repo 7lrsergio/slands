fd81605edc34ecbb4392f19f6cb9f0e5ad891168
7lrsergio <checojlr11@outlook.com>
2025-10-30 11:00:30 -0500

    1

---
+++ SLANDS/script.js
@@ -0,0 +1,13 @@
+{/* <script>
+const rail = document.getElementById('slider');
+
+rail.addEventListener('wheel', e => {
+  if(e.shiftKey) return;
+  rail.scrollBy({ left:e.deltaY, behavior:'smooth' });
+  e.preventDefault();                  // stop page from moving vertically
+});
+</script> */}
+
+
+
+
